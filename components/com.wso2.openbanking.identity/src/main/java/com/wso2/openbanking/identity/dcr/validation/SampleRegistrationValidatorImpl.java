3d970ca29239b44f1f229f1e7aa5810bddce7d5f
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-06-17 22:03:26 +0530

    Modified the RegistrationValidator implementation class to check for the logo_uri parameter in the request object and set the mentioned parameter to the SSA and get the extended registration response object as the response.

--- components/com.wso2.openbanking.identity/src/main/java/com/wso2/openbanking/identity/dcr/validation/SampleRegistrationValidatorImpl.java
+++ components/com.wso2.openbanking.identity/src/main/java/com/wso2/openbanking/identity/dcr/validation/SampleRegistrationValidatorImpl.java
@@ -12,9 +12,16 @@
 
 package com.wso2.openbanking.identity.dcr.validation;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.wso2.openbanking.accelerator.identity.dcr.exception.DCRValidationException;
 import com.wso2.openbanking.accelerator.identity.dcr.model.RegistrationRequest;
+import com.wso2.openbanking.accelerator.identity.dcr.utils.ValidatorUtils;
+import com.wso2.openbanking.accelerator.identity.dcr.validation.DCRCommonConstants;
 import com.wso2.openbanking.accelerator.identity.dcr.validation.DefaultRegistrationValidatorImpl;
+import com.wso2.openbanking.identity.dcr.model.ExtendedRegistrationResponse;
+import com.wso2.openbanking.identity.dcr.model.ExtendedSoftwareStatementBody;
 
 import java.util.Map;
 
@@ -25,7 +32,17 @@ public class SampleRegistrationValidatorImpl extends DefaultRegistrationValidato
 
     @Override
     public void validatePost(RegistrationRequest registrationRequest) throws DCRValidationException {
+        // Call the validatePost() method in the parent class so we don't miss any validations done by the parent class
         super.validatePost(registrationRequest);
+
+        // In the doc they have used a utility class called ValidationUtils. But now that class is missing.
+        // This is the closest thing. So...
+        ValidatorUtils.getValidationViolations(registrationRequest);
+
+        // Check whether the SSA parameters contain a parameter named logo_uri. If not throw the exception.
+        if (!registrationRequest.getSsaParameters().containsKey("logoUri")) {
+            throw new DCRValidationException(DCRCommonConstants.INVALID_SSA, "SSA must contain a logoUri parameter");
+        }
     }
 
     @Override
@@ -45,11 +62,20 @@ public class SampleRegistrationValidatorImpl extends DefaultRegistrationValidato
 
     @Override
     public void setSoftwareStatementPayload(RegistrationRequest registrationRequest, String decodedSSA) {
-        super.setSoftwareStatementPayload(registrationRequest, decodedSSA);
+        // Set the extended software statement instead of the normal one
+        ExtendedSoftwareStatementBody extendedSoftwareStatementBody = new GsonBuilder().create()
+                .fromJson(decodedSSA, ExtendedSoftwareStatementBody.class);
+        registrationRequest.setSoftwareStatementBody(extendedSoftwareStatementBody);
     }
 
     @Override
     public String getRegistrationResponse(Map<String, Object> spMetaData) {
-        return super.getRegistrationResponse(spMetaData);
+        // It seems that the logo_uri is already present in the spMetaData map. So we only have to map the spMetaData
+        // map to the ExtendedRegistrationResponse object.
+        Gson gson = new Gson();
+        JsonElement jsonElement = gson.toJsonTree(spMetaData);
+        ExtendedRegistrationResponse extendedRegistrationResponse =
+                gson.fromJson(jsonElement, ExtendedRegistrationResponse.class);
+        return gson.toJson(extendedRegistrationResponse);
     }
 }
